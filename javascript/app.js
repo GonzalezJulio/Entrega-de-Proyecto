ca28937cefeca535a0b1ef5b0b0dce5a5b066fe6
Julio <aresden113@gmail.com>
2022-09-28 08:29:01 -0300

    javascript

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+const mostrarArticulos = (productos) => {
+    const almacenamientoArticulos = document.getElementById ('producto-contenedor')
+    productos.forEach(producto =>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text">${producto.desc}</p>
+                                <p class="card-text">$ ${producto.precio}</p>
+                                <button type="button" class="btn btn-outline-primary" id="boton${producto.id}">Comprar</button>
+                            </div>
+                        </div>`
+        almacenamientoArticulos.appendChild(div)
+
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener(`click`, () => {
+            carritoCompras(producto.id)
+            alert(`Se agrego el articulo ${producto.nombre} a tu compra`)
+        })
+    })
+}
+
+mostrarArticulos(productos)
---
+++ javascript/carritoIndex.js
@@ -0,0 +1,23 @@
+const carritoArticulos = []
+
+const carritoCompras = (productoId) => {
+
+    const contenedorCarrito = document.getElementById ('carrito-contenedor');
+
+    const articulosCarrito = () => {
+        let producto = productos.find(producto => producto.id == productoId);
+        carritoArticulos.push(productos);
+
+        producto.cantidad = 1;
+
+        let div = document.createElement ('div')
+        div.classList.add('articuloEnCarrito')
+        div.innerHTML = `<p>${producto.nombre}</p>
+                         <p>Precio:$ ${producto.precio}</p>
+                         <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+                         <button id="eliminar${producto.id}" class="boton-eliminar"><i class="fa-solid fa-trash-can"></i></button>
+                         `;
+        contenedorCarrito.appendChild(div)
+    }
+    articulosCarrito();
+}
